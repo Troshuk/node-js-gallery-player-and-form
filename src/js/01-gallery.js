a095de01108648003ce6179a4adf65ea4b9800c6
Denys Troshuk <Troshuk@gmail.com>
2023-10-20 22:17:36 -0400

    task 1 - gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul.gallery');
+
+gallery.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>
+    `
+  )
+  .join('');
+
+new SimpleLightbox('ul.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
