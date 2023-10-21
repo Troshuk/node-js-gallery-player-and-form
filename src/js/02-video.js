ceb3c6c27d988e06856f22bf8bd274ef7d6de257
Denys Troshuk <Troshuk@gmail.com>
2023-10-20 22:40:51 -0400

    video player - local storage + throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_PLAYER_TIME_KEY = 'videoplayer-current-time';
+const storedTime = localStorage.getItem(STORAGE_PLAYER_TIME_KEY);
+
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
+
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
+    localStorage.setItem(STORAGE_PLAYER_TIME_KEY, seconds);
+  }, 1000)
+);
